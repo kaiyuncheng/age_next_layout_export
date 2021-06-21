6ca62fa57df992fb6ed7cab93537f9e1a3e36933
KaiYun Cheng <tanyacheng@gmail.com>
2021-06-21 17:47:16 +0800

    refactor: api url

--- components/utils/axios.js
+++ components/utils/axios.js
@@ -17,7 +17,7 @@ switch (urlHostname) {
     nameUrl = 'https://api-thebetteraging-hardy.businesstoday.com.tw/api/';
     break;
   case `age-next-layout.vercel.app`:
-    nameUrl = 'https://api-thebetteraging-dev.businesstoday.com.tw/api/';
+    nameUrl = 'https://api-thebetteraging-hardy.businesstoday.com.tw/api/';
     break;
   case `localhost`:
     nameUrl = 'https://api-thebetteraging-hardy.businesstoday.com.tw/api/';
